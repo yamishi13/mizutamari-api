af11d0f295e0d0c3803ecd16c18cae7f65c1bbdb
yamishi13 <yamishi.atarashianime@gmail.com>
2014-04-16 07:18:37 -0500

    base prototype on the api an updated tests

--- mizutamari-api.rb
+++ mizutamari-api.rb
@@ -1,6 +1,23 @@
 require 'sinatra'
 require 'json'
+require 'mechanize'
+
+m = Mechanize.new
+page = m.get 'http://www.sepomex.gob.mx/lservicios/seguimientopiezas/emsportal.aspx'
+form = page.form 'Form1'
 
 get '/:number' do
-	
+	content_type :json
+	form.txtNGuia = :number.to_s
+	page = form.click_button(form.button_with(name: /btnFind/))
+	rows = page.search("//table[@id='dg']/tr")
+	limit = page.search("//span[@id='lblTot']").text[-1].to_i
+	ret = {data: []}
+	for r in 1..limit do
+ 		columns = rows[r].search("./td")
+ 		col = {fecha: columns[0].text, origen: columns[2].text, 
+ 			accion: columns[3].text, destino: columns[4].text}
+ 		ret[:data].push col
+ 	end
+ 	ret
 end
--- test.rb
+++ test.rb
@@ -5,3 +5,12 @@ require_relative 'mizutamari-api.rb'
 def app
   Sinatra::Application
 end
+
+describe "page get" do
+	it "should return the package data as json" do
+		get 'EG297596005JP'
+		resp = last_response.body
+		resp.status.must_equal 200
+		resp.data != nil
+	end
+end
